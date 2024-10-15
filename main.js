1e20995675608facfc6bfd46a21778eb4bb2e50c
Andreas Eismann <a.eismann@senbax.de>
2024-10-15 23:56:59 +0200

    feat: First moves are working.

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
 import m from "mithril";
 import tagl from "tagl-mithril";
 
-const { div, h1, p, button } = tagl(m);
+const { div, h1, p, button, svg, circle } = tagl(m);
 
 const use = (v, f) => f(v);
 const range = (n) => Array.from({ length: n }, (_, i) => i);
 
-const coords = (i) => ({ row: i % 8, column: Math.trunc(i / 8) });
-const index = ({ row, column }) => row + column * 8;
+const coords = (i) => ({ column: i % 8, row: Math.trunc(i / 8) });
+const index = ({ row, column }) => column + row * 8;
 const WHITE = 2;
 const BLACK = 3;
 
-const state = { field: [], currentPlayer: 2, selected: -1, possibleMoves: [] };
+const state = {
+  field: [],
+  currentPlayer: undefined,
+  selected: -1,
+  possibleMoves: [],
+};
 
 const current = () => state.currentPlayer;
-const opponent = () => (current() === 2 ? 3 : 2);
+const opponent = () => (current() === WHITE ? BLACK : WHITE);
 
+const isField = (i) => use(coords(i), (c) => (c.row + c.column) % 2);
 const isCurrent = (color) => color > 0 && color % current() === 0;
 const isOpponent = (color) => color > 0 && color % opponent() === 0;
 const nextPlayer = () => (state.currentPlayer = opponent());
 const at = (i) => state.field[i];
-const onBoard = ({ row, column }) => row >= 0 && row < 8 && column >= 0 && column < 8;
+const onBoard = ({ row, column }) =>
+  row >= 0 && row < 8 && column >= 0 && column < 8;
 
 const directions = [
   ({ row, column }) => ({ row: row - 1, column: column - 1 }),
@@ -30,11 +37,11 @@ const directions = [
 ];
 
 const defaultDirections = () =>
-  current() === 2 ? directions.slice(0, 2) : directions.slice(2, 4);
+  current() === WHITE ? directions.slice(0, 2) : directions.slice(2, 4);
 
 const possibleMoves = (idx) => {
   if (!isCurrent(at(idx))) return [];
-  if (at(idx) <= 3) {
+  if (at(idx) <= BLACK) {
     return defaultDirections()
       .map((f) => f(coords(idx)))
       .filter(onBoard)
@@ -45,14 +52,9 @@ const possibleMoves = (idx) => {
 
 const newGame = () => {
   state.field = range(64).map((i) =>
-    use(coords(i), (c) =>
-      (c.row + c.column) % 2 && i < 24
-        ? 3
-        : (c.row + c.column) % 2 && i > 39
-        ? 2
-        : 0
-    )
+    isField(i) && i < 24 ? BLACK : isField(i) && i > 39 ? WHITE : 0
   );
+  state.currentPlayer = WHITE;
 };
 
 newGame();
@@ -69,20 +71,23 @@ const select = (idx) => {
     state.possibleMoves = possibleMoves(state.selected);
     return;
   }
+  if (state.possibleMoves.includes(idx)) {
+    state.field[state.selected] = 0;
+    state.field[idx] = current();
+    state.selected = -1;
+    state.possibleMoves = [];
+    nextPlayer();
+  }
 };
 
 const stone = (vnode) => ({
   view: (vnode) =>
-    m(
-      "svg",
-      {
-        xmlns: "http://www.w3.org/2000/svg",
-        viewBox: "0 0 100 100",
-        // width: "min(10vw, 10vh)",
-        // height: "min(10vw, 10vh)",
-        style: { color: `${vnode.attrs.color}` },
-      },
-      m("circle", { cx: "50", cy: "50", r: "40", fill: `${vnode.attrs.color}` })
+    svg(
+      { viewBox: "0 0 100 100" },
+      circle({ cx: "50", cy: "50", r: "40", fill: `${vnode.attrs.color}` }),
+      vnode.attrs.queen
+        ? circle({ cx: "50", cy: "50", r: "14", fill: `${"white"}` })
+        : null
     ),
 });
 
@@ -90,18 +95,20 @@ m.mount(document.body, {
   view: () => [
     div.board(
       state.field.map((i, idx) =>
-        div.field[
-          use(coords(idx), (c) => ((c.row + c.column) % 2 ? "black" : "white"))
-        ][state.selected === idx ? "selected" : ""][state.possibleMoves.includes(idx)?"possible":""](
+        div.field[use(coords(idx), (c) => (isField(idx) ? "black" : "white"))][
+          state.selected === idx ? "selected" : ""
+        ][state.possibleMoves.includes(idx) ? "possible" : ""](
+          { onclick: () => select(idx) },
           i > 0
             ? div(
-                { onclick: () => select(idx) },
-                m(stone, { color: i === 2 ? "salmon" : "black" })
+                m(stone, {
+                  color: i === 2 ? "salmon" : "black",
+                  queen: i > BLACK,
+                })
               )
-            : undefined // use(coords(idx), (c) => c.row +","+ c.column)
+            : undefined // use(coords(idx), (c) => c.row + "," + c.column)
         )
       )
     ),
-    button({ onclick: nextPlayer }, "Next player"),
   ],
 });
